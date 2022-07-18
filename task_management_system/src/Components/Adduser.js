4275e6e191c52d0da02e80c7d32752e279396c59
kamal798 <Kamalnature772@gmail.com>
2022-07-18 14:39:41 +0545

    Edited to fit the project

---
+++ task_management_system/src/Components/Adduser.js
@@ -0,0 +1,19 @@
+import React from "react";
+ 
+const AddUSer = () => {
+    return(
+        <div>
+            <div className = "user-header">
+                <div className="content-header">
+                    <h2> USER LIST</h2>
+                    <button className= "add-btn">Add User</button>
+                </div>
+            </div>
+            <div className = "user-table">
+
+            </div>
+        </div>
+
+    );
+}
+export default AddUSer
