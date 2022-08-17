6a93094917cae7a067e34c7a8a0e3bddeaa0cf12
sachikamat <sachikamat1@gmail.com>
2022-08-17 13:53:33 +0545

    cors origin added

--- Server/app.js
+++ Server/app.js
@@ -5,7 +5,7 @@ require("./db/conn");
 const cors = require("cors");
 //
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:['http://localhost:3000','http://192.168.0.121:3000'], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
